443435ff8aaf0b80c8d522b4780bc38c359ff9d5
bryce <bryce.hoehn@icloud.com>
2024-05-11 09:41:26 -0400

    stuff

--- generatedb.py
+++ generatedb.py
@@ -9,10 +9,18 @@ cur.execute('CREATE TABLE IF NOT EXISTS spells (name)')
 with open('spells-phb.json') as json_file:
    spells = json.load(json_file)
    for spell in spells['spell']:
+      name = spell['name']
+
+      cur.execute('SELECT * FROM spells')
+      columns = list(map(lambda x: x[0], cur.description))
+      if not 'name' in columns:
+         cur.execute(f'ALTER TABLE spells ADD COLUMN {name}')
+      cur.execute('INSERT INTO spells (name) VALUES (?)', (spell['name'],))
+
       for i in spell:
-         cur.execute('SELECT * FROM spells')
-         columns = list(map(lambda x: x[0], cur.description))
          if not i in columns:
             cur.execute(f'ALTER TABLE spells ADD COLUMN {i}')
-         cur.execute(f'INSERT INTO spells ({i}) VALUES (?, ?)', spell[i])
 
+         cur.execute(f'UPDATE spells SET {i} = ? WHERE name=?', (str(spell[i]), name))
+
+con.commit()

e052801f06093ba918899a1e8a06e8a92199122a
bryce <bryce.hoehn@icloud.com>
2024-05-11 01:03:09 -0400

    stuff

--- generatedb.py
+++ generatedb.py
@@ -7,18 +7,12 @@ cur = con.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS spells (name)')
 
 with open('spells-phb.json') as json_file:
-    spells = json.load(json_file)
-    for spell in spells['spell']:
-       for i in spell:
-          print(i)
-          try:
-             cur.execute(f"""
-                      ALTER TABLE spells
-                      ADD {i}""")
-          except sqlite3.OperationalError:
-             pass
-          
-          cur.execute(f"""
-                      INSERT INTO spells ("{i}")
-                      VALUES ("{spell[i]}");
-                      """)
+   spells = json.load(json_file)
+   for spell in spells['spell']:
+      for i in spell:
+         cur.execute('SELECT * FROM spells')
+         columns = list(map(lambda x: x[0], cur.description))
+         if not i in columns:
+            cur.execute(f'ALTER TABLE spells ADD COLUMN {i}')
+         cur.execute(f'INSERT INTO spells ({i}) VALUES (?, ?)', spell[i])
+

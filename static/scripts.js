dbecf52162af8c460c3a502cf9fcdd136e7e1ac7
bryce <bryce.hoehn@icloud.com>
2024-05-24 16:48:50 -0400

    idk

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,42 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
-// Get all elements with the class name 'md'
-var md = document.getElementsByClassName('md');
+document.querySelectorAll('.md').forEach((element) => {
+  let currentContent = element.innerHTML;
+  element.innerHTML = marked.parse(currentContent);
+});
 
-// Iterate over each element and modify its innerHTML
-for (var i = 0; i < md.length; i++) {
-  var currentContent = md[i].innerHTML;
-  md[i].innerHTML = marked.parse(currentContent);
+if (window.location.pathname == '/create/description') {
+  let backgrounds;
+
+  function fetchJSONData() {
+    fetch("/backgrounds.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error
+            (`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) =>
+        backgrounds = data)
+      .catch((error) =>
+        console.error("Unable to fetch data:", error));
+  }
+
+  var background = document.querySelector("#background");
+
+  fetchJSONData();
+
+  background.addEventListener("change", function () {
+    console.log(background.selectedIndex);
+    if (background.selectedIndex == 0) {
+      document.querySelector('#background_desc').innerHTML = "";
+      document.querySelector('#skills').innerHTML = "";
+    }
+
+    else {
+      document.querySelector('#background_desc').innerHTML = marked.parse(backgrounds['results'][background.selectedIndex - 1]['desc']);
+      document.querySelector('#skills').innerHTML = backgrounds['results'][background.selectedIndex - 1]['skill_proficiencies']
+    }
+  });
 }
